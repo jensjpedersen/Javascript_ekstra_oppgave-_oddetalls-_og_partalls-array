f55a47f89815a77306131e99c9ab1f62cc532ce6
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-11-27 09:40:31 +0100

    Added 4.

--- script.js
+++ script.js
@@ -13,19 +13,20 @@ for (let i = 0; i <= 99; i++) {
 
 
 
+function sort_odd_even(arr) {
+    let even_arr = arr.filter( (e) => e % 2 === 0 ); 
+    let odd_arr = arr.filter( (e) => e % 2 != 0 ); 
+    return {odd: odd_arr, evens: even_arr}; 
+}
 // TODO - Do not use double loops. 
-let new_arr = arr.filter(function (e) {
-    return e % 2 == 0
-})
-
-let even_arr = arr.filter( (e) => e % 2 === 0 )
-let odd_arr = arr.filter( (e) => e % 2 != 0 )
-
-let odd_even_obj = {odd: odd_arr, evens: even_arr} 
 
 
-console.log(odd_even_obj);
-console.log(even_arr.length + odd_arr.length);
-// console.log(new_arr);
-//
-const {odds, evens} 
+
+const {odd, evens} = sort_odd_even(arr);
+console.log(`# odds: ${odd.length}, # evens ${evens.length}`);
+
+
+
+
+
+
