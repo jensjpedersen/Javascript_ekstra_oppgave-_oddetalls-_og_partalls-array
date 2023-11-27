063e54e1d8dc4cac7c0140bee631ebf905239cdd
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-11-27 13:01:33 +0100

    Updates

--- test.js
+++ test.js
@@ -4,6 +4,5 @@
 // Create gh pages from repo in cd path
 let ghpages = require('gh-pages');
 const execution_dir = process.cwd();
-ghpages.publish(execution_dir, function(err) {console.log(err)})
-
+ghpages.publish(execution_dir, {branch: 'main'}, function (err) {console.log(err)}); 
 
